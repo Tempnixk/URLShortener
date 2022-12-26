d78f7fe6ba86c67a2c63a0034f686aa07f194dce
Hyunmin Cho <chm49@naver.com>
2022-12-26 22:27:34 +0900

    [feat] Read 기능 구현

--- urlshortenerapp/views.py
+++ urlshortenerapp/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+topics = [
+    {'id':1, 'title':'Shorten', 'body': 'This is URL Shortener'},
+    {'id':2, 'title':'Shorten URL List', 'body': 'Shorten URL List'},
+    {'id':3, 'title':'Analysis', 'body': 'Analysis'},
+    
+]
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol =''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">URLShortener</a></h1>
+        <ol>
+            {ol}
+        </ol>
+        {articleTag}
+        
+    </body>
+    </html>
+    '''
 
 # Create your views here.
+def index(request):
+    article = '''
+    <h2>Welcome</h2>
+    Hello!
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+
+
+def read(request, id):
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id) :
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+    
+def create(request):
+    return HttpResponse('Create')
